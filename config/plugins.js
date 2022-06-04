d7cf2e539aca8c5402b35094f6595430dabe9280
talabidele <talabibamidele7@gmail.com>
2022-06-05 00:00:29 +0100

    Setting up mail gun

--- config/plugins.js
+++ config/plugins.js
@@ -9,4 +9,25 @@ module.exports = ({ env }) => ({
     },
   },
   // ...
+
+  // email: {
+  //   provider: "sendmail",
+  //   settings: {
+  //     defaultFrom: "no-reply@fitfixam.com",
+  //     defaultReplyTo: "info@fitfixam.com",
+  //   },
+  // },
+
+  email: {
+    provider: "mailgun",
+    providerOptions: {
+      apiKey: env("MAILGUN_API_KEY"),
+      domain: env("MAILGUN_DOMAIN"), //Required if you have an account with multiple domains
+      // host: env("MAILGUN_HOST", "api.mailgun.net"), //Optional. If domain region is Europe use 'api.eu.mailgun.net'
+    },
+    settings: {
+      defaultFrom: "no-reply@fitfixam.com",
+      defaultReplyTo: "info@fitfixam.com",
+    },
+  },
 });
