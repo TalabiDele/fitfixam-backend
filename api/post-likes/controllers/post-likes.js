5de1244a6df6872d0c0092191787bd3dc221449c
talabidele <talabibamidele7@gmail.com>
2022-06-27 17:13:22 +0100

    Update

--- api/post-likes/controllers/post-likes.js
+++ api/post-likes/controllers/post-likes.js
@@ -8,18 +8,17 @@
 const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
-  async create(ctx) {
-    let entity;
-    const { user } = ctx.state;
-    const { post } = ctx.request.body;
-    // const found = await strapi.services.postlikes.findOne({
-    //   user: user.id,
-    //   post,
-    // });
-
-    // if (found) {
-    //   ctx.throw(400, "You already liked this post");
-    // }
-    // return sanitizeEntity(entity, { model: strapi.models.post_likes });
-  },
+  // async create(ctx) {
+  // let entity;
+  // const { user } = ctx.state;
+  // const { post } = ctx.request.body;
+  // const found = await strapi.services.postlikes.findOne({
+  //   user: user.id,
+  //   post,
+  // });
+  // if (found) {
+  //   ctx.throw(400, "You already liked this post");
+  // }
+  // return sanitizeEntity(entity, { model: strapi.models.post_likes });
+  // },
 };
