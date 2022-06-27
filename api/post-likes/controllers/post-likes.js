063b3f8a2fb4e9d6871da8fb6600801ad6240fb9
talabidele <talabibamidele7@gmail.com>
2022-06-27 14:24:00 +0100

    Removed like error

--- api/post-likes/controllers/post-likes.js
+++ api/post-likes/controllers/post-likes.js
@@ -12,14 +12,14 @@ module.exports = {
     let entity;
     const { user } = ctx.state;
     const { post } = ctx.request.body;
-    const found = await strapi.services.likes.findOne({
-      user: user.id,
-      post,
-    });
+    // const found = await strapi.services.postlikes.findOne({
+    //   user: user.id,
+    //   post,
+    // });
 
-    if (found) {
-      ctx.throw(400, "You already liked this post");
-    }
+    // if (found) {
+    //   ctx.throw(400, "You already liked this post");
+    // }
     return sanitizeEntity(entity, { model: strapi.models.post_likes });
   },
 };
